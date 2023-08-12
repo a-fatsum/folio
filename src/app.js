1de980ba649e8e6dfadf690bc3efa8b01cd350be
Mustafa <mustafaabdelati@gmail.come.com>
2023-08-12 13:57:42 +1000

    updated styles.css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const hamburgerButton = document.getElementById('hamburger');
-const navList =  document.getElementById('nav-list');
+const hamburgerButton = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
+const frame = document.querySelector(".frame ");
 
 function toggleButton() {
-    navList.classList.toggle('show')
+  navList.classList.toggle("show");
+  frame.classList.toggle("frame-hide");
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener("click", toggleButton);
