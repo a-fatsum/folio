19b78e8c3711266d588c934a81c1226ac426dd7c
Mustafa Abdelati <mustafaabdelati@gmail.com>
2023-08-31 18:37:44 +1000

    added hamburger menu

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
-const frame = document.querySelector(".frame ");
+// const frame = document.querySelector(".frame ");
 
 function toggleButton() {
   navList.classList.toggle("show");
-  frame.classList.toggle("frame-hide");
+  // frame.classList.toggle("frame-hide");
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
 
 // subsection
-const subSection = document.querySelector(".sub-section");
+// const subSection = document.querySelector(".sub-section");
 //
 // Education
-const education = document.getElementById("education");
+// const education = document.getElementById("education");
 //
 // skills
-const skills = document.getElementById("skills");
+// const skills = document.getElementById("skills");
 //
-const infoDisplay = function () {
-  const education = document.getElementById("education");
-  education.classList.toggle("education-show");
-  education.classList.toggle("about-button");
-  //
-  const skills = document.getElementById("skills");
-  skills.classList.toggle("skills-show");
-  skills.classList.toggle("about-button");
-};
+// const infoDisplay = function () {
+//   const education = document.getElementById("education");
+//   education.classList.toggle("education-show");
+//   education.classList.toggle("about-button");
+//   //
+//   const skills = document.getElementById("skills");
+//   skills.classList.toggle("skills-show");
+//   skills.classList.toggle("about-button");
+// };
 
-subSection.addEventListener("mouseover", infoDisplay);
-// //
-subSection.addEventListener("mouseout", infoDisplay);
+// subSection.addEventListener("mouseover", infoDisplay);
+// // //
+// subSection.addEventListener("mouseout", infoDisplay);
