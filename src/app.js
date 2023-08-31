272927e02c0fc9541a5911c1a75e1fa8bc99917a
Mustafa Abdelati <mustafaabdelati@gmail.com>
2023-08-31 23:02:26 +1000

    added hamburger menu

--- src/app.js
+++ src/app.js
@@ -1,33 +1,6 @@
-const hamburgerButton = document.getElementById("hamburger");
-const navList = document.getElementById("nav-list");
-// const frame = document.querySelector(".frame ");
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
 
-function toggleButton() {
-  navList.classList.toggle("show");
-  // frame.classList.toggle("frame-hide");
-}
-
-hamburgerButton.addEventListener("click", toggleButton);
-
-// subsection
-// const subSection = document.querySelector(".sub-section");
-//
-// Education
-// const education = document.getElementById("education");
-//
-// skills
-// const skills = document.getElementById("skills");
-//
-// const infoDisplay = function () {
-//   const education = document.getElementById("education");
-//   education.classList.toggle("education-show");
-//   education.classList.toggle("about-button");
-//   //
-//   const skills = document.getElementById("skills");
-//   skills.classList.toggle("skills-show");
-//   skills.classList.toggle("about-button");
-// };
-
-// subSection.addEventListener("mouseover", infoDisplay);
-// // //
-// subSection.addEventListener("mouseout", infoDisplay);
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("hide");
+});
