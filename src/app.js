213aea2479539635fef0fae4d82426cd553c49a9
Mustafa Abdelati <mustafaabdelati@gmail.com>
2023-09-03 22:32:01 +1000

    added contact me form

--- src/app.js
+++ src/app.js
@@ -4,3 +4,21 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("hide");
 });
+
+// contact form
+
+// const form = document.getElementById("form");
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "mustafaabdelati@gmail.com",
+//     Password: "CuSO45H2O",
+//     To: "mustafaabdelati@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "New enquiry",
+//     Body: "And this is the body",
+//   }).then((message) => alert(message));
+// }
+
+// form.addEventListener("submit", sendEmail);
