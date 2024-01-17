886c41eb16a93a910ae14164b76533ea6fa13f04
a-fat-sum <mustafaabdelati@gmail.com>
2024-01-17 19:49:15 +1100

    updated styles .css - created index-3.html to experement with a different layout

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document
+        .querySelectorAll(".project-card")[0]
+        .classList.add("fade-in-right");
+      document
+        .querySelectorAll(".project-card")[1]
+        .classList.add("fade-in-left");
+      document
+        .querySelectorAll(".project-card")[2]
+        .classList.add("fade-in-right");
+    } else {
+      document
+        .querySelectorAll(".project-card")[0]
+        .classList.remove("fade-in-right");
+      document
+        .querySelectorAll(".project-card")[1]
+        .classList.remove("fade-in-left");
+      document
+        .querySelectorAll(".project-card")[2]
+        .classList.remove("fade-in-right");
+    }
+  });
+});
 
+observer.observe(document.querySelector(".sub-section-alternative"));
+// observer.observe(document.querySelector(".project-card-right"));
+// observer.observe(document.querySelector(".project-card-left"));
